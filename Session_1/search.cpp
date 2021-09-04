cca402eb7c5eb2b4289f5a1e379e878f390573f1
Arsenic <54987647+Arsenic-ATG@users.noreply.github.com>
2021-09-04 12:48:18 +0530

    Session_1: update search.cpp

--- Session_1/search.cpp
+++ Session_1/search.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
-int linear_search(std::vector<int> &v, int target)
+int search(std::vector<int> &arr, int target)
 {
 	int i;
-	for (i = 0; i < v.size(); i++)
-		if (v[i] == target)
+	for (i = 0; i < arr.size(); i++)
+		if (arr[i] == target)
 			return i;
 	return -1;
 }
 
 int main(void)
 {
-	std::vector<int> v = { 1,2,3,4,5 };
+	std::vector<int> v = {1,2,3,4,5};
 	int target = 3;
 
-	int index = linear_search (v, target);
+	int index = search (v, target);
 	if (index == -1)
 		std::cout<<"element not found \n";
 	else
